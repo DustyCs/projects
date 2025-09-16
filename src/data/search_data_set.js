097fdd3083c2718a2a5703baf10bbb5b18349b28
DustyC <123617229+DustyCs@users.noreply.github.com>
2025-09-16 17:01:06 +0800

    Nav

---
+++ src/data/search_data_set.js
@@ -0,0 +1,8 @@
+export const searchDataset = [
+    { id: 1, label: "A Slime's Small Hometown", type: "games", url: "/games" },
+    { id: 2, label: "PostMaker", type: "games", url: "/games" },
+    { id: 3, label: "Blog", type: "posts", url: "/" },
+    { id: 4, label: "Projects", type: "projects", url: "/projects" },
+    { id: 5, label: "Contact Me - Form", type: "contact", url: "/projects" },
+    { id: 6, label: "License Summary", type: "license", url: "/games" },
+  ];
