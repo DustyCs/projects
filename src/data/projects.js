07d16301f0cfcfbc1b742c77c4ab55b490a95d2d
DustyC <123617229+DustyCs@users.noreply.github.com>
2025-09-17 13:54:30 +0800

    [DEV] Updated Nav and Dynamic Template

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,24 +1,176 @@
+import Homepage333 from "../assets/projects/lifetime/Homepage333.PNG"
+import Activity from "../assets/projects/lifetime/Activity33.PNG"
+import AlarmPage from "../assets/projects/lifetime/Alarmmm page.PNG"
+
+import CrusadersSkirmish from "../assets/projects/GameLatest.png"
+
 export const projects = [
-    {
-      title: "Project 1",
-      slug: "project-1",
-      description: "This is a description for Project 1.",
-      images: ["https://via.placeholder.com/150"],
-      links: [
-        { label: "GitHub", url: "https://github.com" },
-        { label: "Live Demo", url: "https://example.com" }
-      ],
-      tags: ["tag1", "tag2"]
-    },
-    {
-      title: "Project 2",
-      slug: "project-2",
-      description: "This is a description for Project 2.",
-      images: ["https://via.placeholder.com/150"],
-      links: [
-        { label: "GitHub", url: "https://github.com" },
-        { label: "Live Demo", url: "https://example.com" }
-      ],
-      tags: ["tag3", "tag4"]
-    }
-]
+  {
+    title: "LifeTime",
+    slug: "lifetime",
+    description: "LifeTime is a cross-platform website with an android application that not only allows you to track your daily activities and goals but also have an AI Assistant that can help you with your daily tasks and planning",
+    gallery: [
+      {
+        image: [Homepage333, Activity],
+        description: "This is the first image"
+      },
+      {
+        image: [AlarmPage],
+        description: "This is the second image"
+      }
+    ],
+    links: [
+      { label: "Visit", url: "https://lifetime-schedules.web.app" }
+    ],
+    tags: ["React", "FramerMotion", "TailwindCSS", "Firebase", "AI", "NodeJS", "ExpressJS", "MongoDB", "Android", "Kivy", "App"],
+    style: "column",
+    theme: "dark"
+  },
+  {
+    title: "Crusader's Skirmish",
+    slug: "crusaders-skirmish",
+    description: "Crusader's Skirmish is an upcoming card clash online game on the browser playable on both desktop and mobile! Using Socket.IO",
+    gallery: [
+      {
+        image: [CrusadersSkirmish],
+        description: "Another feature screenshot"
+      },
+      {
+        image: ["https://via.placeholder.com/150"],
+        description: "Final demo screenshot"
+      }
+    ],
+    links: [
+      { label: "Visit", url: "lifetime-schedules.web.app" }
+    ],
+    tags: ["React", "Socket.IO", "ExpressJS", "Jest", "NodeJS", "MongoDB", "TailwindCSS", "Firebase"],
+    style: "column",
+    theme: "white"
+  },
+  {
+    title: "Rock Paper Scissors Online",
+    slug: "rps-online",
+    description: "RPS Online is made by API Polling to check in interval the status of both players",
+    gallery: [
+      {
+        image: [CrusadersSkirmish],
+        description: "Another feature screenshot"
+      },
+      {
+        image: ["https://via.placeholder.com/150"],
+        description: "Final demo screenshot"
+      }
+    ],
+    links: [
+      { label: "Visit", url: "lifetime-schedules.web.app" }
+    ],
+    tags: ["React", "Socket.IO", "ExpressJS", "Jest", "NodeJS", "MongoDB", "TailwindCSS", "Firebase"],
+    style: "column",
+    theme: "white"
+  },
+  {
+    title: "RockPaperScissors PHP",
+    slug: "rps-php",
+    description: "One of my very first project where I learned PHP, MySQL, JQuery and Ajax",
+    gallery: [
+      {
+        image: [CrusadersSkirmish],
+        description: "Another feature screenshot"
+      },
+      {
+        image: ["https://via.placeholder.com/150"],
+        description: "Final demo screenshot"
+      }
+    ],
+    links: [
+      { label: "Visit", url: "lifetime-schedules.web.app" }
+    ],
+    tags: ["React", "Socket.IO", "ExpressJS", "Jest", "NodeJS", "MongoDB", "TailwindCSS", "Firebase"],
+    style: "column",
+    theme: "white"
+  },
+  {
+    title: "Silver Squares Online",
+    slug: "silver-squares",
+    description: "An Imitation of the American game show of Golden Balls. With the use of Socket.IO",
+    gallery: [
+      {
+        image: [CrusadersSkirmish],
+        description: "Another feature screenshot"
+      },
+      {
+        image: ["https://via.placeholder.com/150"],
+        description: "Final demo screenshot"
+      }
+    ],
+    links: [
+      { label: "Visit", url: "lifetime-schedules.web.app" }
+    ],
+    tags: ["React", "Socket.IO", "ExpressJS", "Jest", "NodeJS", "MongoDB", "TailwindCSS", "Firebase"],
+    style: "column",
+    theme: "white"
+  },
+  {
+    title: "Antique Portfolio",
+    slug: "silver-squares",
+    description: "An Imitation of the American game show of Golden Balls. With the use of Socket.IO",
+    gallery: [
+      {
+        image: [CrusadersSkirmish],
+        description: "Another feature screenshot"
+      },
+      {
+        image: ["https://via.placeholder.com/150"],
+        description: "Final demo screenshot"
+      }
+    ],
+    links: [
+      { label: "Visit", url: "lifetime-schedules.web.app" }
+    ],
+    tags: ["React", "Socket.IO", "ExpressJS", "Jest", "NodeJS", "MongoDB", "TailwindCSS", "Firebase"],
+    style: "column",
+    theme: "white"
+  },
+  {
+    title: "Portfolio NextJS",
+    slug: "silver-squares",
+    description: "An Imitation of the American game show of Golden Balls. With the use of Socket.IO",
+    gallery: [
+      {
+        image: [CrusadersSkirmish],
+        description: "Another feature screenshot"
+      },
+      {
+        image: ["https://via.placeholder.com/150"],
+        description: "Final demo screenshot"
+      }
+    ],
+    links: [
+      { label: "Visit", url: "lifetime-schedules.web.app" }
+    ],
+    tags: ["React", "Socket.IO", "ExpressJS", "Jest", "NodeJS", "MongoDB", "TailwindCSS", "Firebase"],
+    style: "column",
+    theme: "white"
+  },
+  {
+    title: "PostMaker PHP",
+    slug: "silver-squares",
+    description: "An Imitation of the American game show of Golden Balls. With the use of Socket.IO",
+    gallery: [
+      {
+        image: [CrusadersSkirmish],
+        description: "Another feature screenshot"
+      },
+      {
+        image: ["https://via.placeholder.com/150"],
+        description: "Final demo screenshot"
+      }
+    ],
+    links: [
+      { label: "Visit", url: "lifetime-schedules.web.app" }
+    ],
+    tags: ["React", "Socket.IO", "ExpressJS", "Jest", "NodeJS", "MongoDB", "TailwindCSS", "Firebase"],
+    style: "column",
+    theme: "white"
+  },
+];
