87cf4700b8f42f666c58a70380c33d28debff256
DustyC <123617229+DustyCs@users.noreply.github.com>
2025-09-17 21:53:43 +0800

    [DEV] Added more data about projects to be updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,15 @@
 import Homepage333 from "../assets/projects/lifetime/Homepage333.PNG"
 import Activity from "../assets/projects/lifetime/Activity33.PNG"
 import AlarmPage from "../assets/projects/lifetime/Alarmmm page.PNG"
-
+import RPSOnline from "../assets/projects/RPSOnline.png"
 import CrusadersSkirmish from "../assets/projects/GameLatest.png"
+import RpsPHP from "../assets/projects/PHP RPS/2.jpg"
+import RpsPHP2 from "../assets/projects/PHP RPS/1.PNG"
+import RpsPHP3 from "../assets/projects/PHP RPS/3.jpg"
+import SilverSquares from "../assets/projects/Silver Squares/1.PNG"
+import PostMaker from "../assets/projects/PostMaker/PostMaker.PNG"
+import NextPort from "../assets/projects/Next Portfolio/1.PNG"
+import AntiquePort from "../assets/projects/antique modern/1.PNG"
 
 export const projects = [
   {
@@ -53,7 +60,7 @@ export const projects = [
     description: "RPS Online is made by API Polling to check in interval the status of both players",
     gallery: [
       {
-        image: [CrusadersSkirmish],
+        image: [RPSOnline],
         description: "Another feature screenshot"
       },
       {
@@ -69,23 +76,27 @@ export const projects = [
     theme: "white"
   },
   {
-    title: "RockPaperScissors PHP",
+    title: "Rock Paper Scissors PHP",
     slug: "rps-php",
     description: "One of my very first project where I learned PHP, MySQL, JQuery and Ajax",
     gallery: [
       {
-        image: [CrusadersSkirmish],
+        image: [RpsPHP],
         description: "Another feature screenshot"
       },
       {
-        image: ["https://via.placeholder.com/150"],
+        image: [RpsPHP3],
+        description: "Final demo screenshot"
+      },
+      {
+        image: [RpsPHP2],
         description: "Final demo screenshot"
       }
     ],
     links: [
       { label: "Visit", url: "lifetime-schedules.web.app" }
     ],
-    tags: ["React", "Socket.IO", "ExpressJS", "Jest", "NodeJS", "MongoDB", "TailwindCSS", "Firebase"],
+    tags: ["PHP", "MySQL", "JQuery", "Ajax"],
     style: "column",
     theme: "white"
   },
@@ -95,7 +106,7 @@ export const projects = [
     description: "An Imitation of the American game show of Golden Balls. With the use of Socket.IO",
     gallery: [
       {
-        image: [CrusadersSkirmish],
+        image: [SilverSquares],
         description: "Another feature screenshot"
       },
       {
@@ -112,11 +123,11 @@ export const projects = [
   },
   {
     title: "Antique Portfolio",
-    slug: "silver-squares",
-    description: "An Imitation of the American game show of Golden Balls. With the use of Socket.IO",
+    slug: "antique-portfolio",
+    description: "A Portfolio made with React and TailwindCSS",
     gallery: [
       {
-        image: [CrusadersSkirmish],
+        image: [AntiquePort],
         description: "Another feature screenshot"
       },
       {
@@ -127,17 +138,17 @@ export const projects = [
     links: [
       { label: "Visit", url: "lifetime-schedules.web.app" }
     ],
-    tags: ["React", "Socket.IO", "ExpressJS", "Jest", "NodeJS", "MongoDB", "TailwindCSS", "Firebase"],
+    tags: ["React", "TailwindCSS", "FramerMotion"],
     style: "column",
     theme: "white"
   },
   {
     title: "Portfolio NextJS",
-    slug: "silver-squares",
-    description: "An Imitation of the American game show of Golden Balls. With the use of Socket.IO",
+    slug: "next-portfolio",
+    description: "A Portfolio made with NextJS and TailwindCSS",
     gallery: [
       {
-        image: [CrusadersSkirmish],
+        image: [NextPort],
         description: "Another feature screenshot"
       },
       {
@@ -148,17 +159,17 @@ export const projects = [
     links: [
       { label: "Visit", url: "lifetime-schedules.web.app" }
     ],
-    tags: ["React", "Socket.IO", "ExpressJS", "Jest", "NodeJS", "MongoDB", "TailwindCSS", "Firebase"],
+    tags: ["NextJS", "TailwindCSS", "FramerMotion"],
     style: "column",
     theme: "white"
   },
   {
     title: "PostMaker PHP",
-    slug: "silver-squares",
-    description: "An Imitation of the American game show of Golden Balls. With the use of Socket.IO",
+    slug: "postmaker",
+    description: "I created a PostMaker for my old planned blog that takes only JSON data",
     gallery: [
       {
-        image: [CrusadersSkirmish],
+        image: [PostMaker],
         description: "Another feature screenshot"
       },
       {
@@ -169,7 +180,7 @@ export const projects = [
     links: [
       { label: "Visit", url: "lifetime-schedules.web.app" }
     ],
-    tags: ["React", "Socket.IO", "ExpressJS", "Jest", "NodeJS", "MongoDB", "TailwindCSS", "Firebase"],
+    tags: ["PHP", "JQuery"],
     style: "column",
     theme: "white"
   },
